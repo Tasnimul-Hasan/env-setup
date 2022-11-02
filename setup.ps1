b612ddaeaa7d1cd8c103b16dbd64f3016e09e054
Tasnimul Hasan <589.dreamer.589@gmail.com>
2022-11-02 22:57:04 +0600

    fixed typo

--- setup.ps1
+++ setup.ps1
@@ -15,17 +15,17 @@ else {
   Write-Host "The profile @ [$PROFILE] has been created and old profile removed."
 }
 
-# Install necessary plugins
+<# # Install necessary plugins
 Install-Module -Name Terminal-Icons
 Install-Module -Name z
 Install-Module -Name PSReadLine -AllowPrerelease -Scope CurrentUser -Force -SkipPublisherCheck
 Install-Module -Name posh-git
 
 # Install oh-my-posh
-winget install -e --accept-source-agreements --accept-package-agreements JanDeDobbeleer.OhMyPosh
+winget install -e --accept-source-agreements --accept-package-agreements JanDeDobbeleer.OhMyPosh #>
 
 # Restart profile
 & $profile
 
 # download my modified star omp theme and put it inside the themes folder
-Invoke-RestMethod https://github.com/Tasnimul-Hasan/env-setup/raw/main/modified-posh-themes/star-modified.omp.json -o $PROFILE$env:POSH_THEMES_PATH\star-modified.omp.json
+Invoke-RestMethod https://github.com/Tasnimul-Hasan/env-setup/raw/main/modified-posh-themes/star-modified.omp.json -o $env:POSH_THEMES_PATH\star-modified.omp.json
