454bad1e09d3e966e7334ce176e21aaa8c4f9415
Tasnimul Hasan <589.dreamer.589@gmail.com>
2022-11-19 01:52:06 +0600

    alias expanding in psreadline

--- PowerShell/Scripts/psreadline_config.ps1
+++ PowerShell/Scripts/psreadline_config.ps1
@@ -202,3 +202,35 @@ Set-PSReadLineKeyHandler -Key Backspace `
         }
     }
 }
+
+Set-PSReadLineKeyHandler -Key "Alt+%" `
+    -BriefDescription ExpandAliases `
+    -LongDescription "Replace all aliases with the full command" `
+    -ScriptBlock {
+    param($key, $arg)
+
+    $ast = $null
+    $tokens = $null
+    $errors = $null
+    $cursor = $null
+    [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$ast, [ref]$tokens, [ref]$errors, [ref]$cursor)
+
+    $startAdjustment = 0
+    foreach ($token in $tokens) {
+        if ($token.TokenFlags -band [TokenFlags]::CommandName) {
+            $alias = $ExecutionContext.InvokeCommand.GetCommand($token.Extent.Text, 'Alias')
+            if ($alias -ne $null) {
+                $resolvedCommand = $alias.ResolvedCommandName
+                if ($resolvedCommand -ne $null) {
+                    $extent = $token.Extent
+                    $length = $extent.EndOffset - $extent.StartOffset
+                    [Microsoft.PowerShell.PSConsoleReadLine]::Replace(
+                        $extent.StartOffset + $startAdjustment,
+                        $length,
+                        $resolvedCommand)
+                    $startAdjustment += ($resolvedCommand.Length - $length)
+                }
+            }
+        }
+    }
+}
