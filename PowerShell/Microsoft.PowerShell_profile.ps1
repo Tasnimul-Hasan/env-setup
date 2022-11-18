6ed2caefe18931d3f2933fc428f5a84f10cba593
Tasnimul Hasan <589.dreamer.589@gmail.com>
2022-11-19 01:35:53 +0600

    i don't understand symlinks

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,16 @@
 using namespace System.Management.Automation
 using namespace System.Management.Automation.Language
 
+# terminal decorations
+Import-Module -Name z
+Import-Module -Name Terminal-Icons
+Import-Module -Name posh-git
+
+oh-my-posh init pwsh --config $env:POSH_THEMES_PATH\star-modified.omp.json | Invoke-Expression
+
+# Uncomment the line below if your posh theme looks broken on first launch
+# Clear-Host
+
 # aliases
 Set-Alias -Name vim -Value nvim
 Set-Alias -Name lua -Value lua53
@@ -56,3 +66,13 @@ set-alias -name gpush -value Git-Commit
 
 # loading PSReadLine configurations
 psreadline_config
+
+# Import the Chocolatey Profile that contains the necessary code to enable
+# tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion
+# for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+    Import-Module "$ChocolateyProfile"
+}
